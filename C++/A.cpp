78c1a11f2b42b39346d372de001d56e25fd73d19
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-01 17:41:04 +0530

    Update A.cpp

--- C++/A.cpp
+++ C++/A.cpp
@@ -1,6 +1,6 @@
-#include <iostream> // Includes the input/output stream library
+#include <iostream> 
 
-int main() { // The main function, where program execution begins
-    std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" to the console
-    return 0; // Indicates successful program execution
+int main() { 
+    std::cout << "Hello, World Sandeep Yadav!" << std::endl; 
+    return 0; 
 }
