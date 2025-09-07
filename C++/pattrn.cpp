292fa1d953d6961c6f673d6d38845b84447d5933
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-07 16:51:39 +0530

    pattern

--- C++/pattrn.cpp
+++ C++/pattrn.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 int main()
 {
-   int n,m;
-   cout<<"enter row :";
-   cin>>n;
-   cout<<"enter column";
-   cin>>m;
-   
-   
-   
+    int n,m;
+    cout<<"enter the row :";
+    cin>>n;
+    cout<<"e ter the column ";
+    cin>>m;
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
         {
-            cout<<10<<" ";
+            cout<<i<<" ";
         }
         cout<<endl;
     }
