2b17b5587162c3572eb16ad9f66f4f6e64bd305c
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-08 19:20:49 +0530

    simple_pattern9

--- C++/simple_pattern8.cpp
+++ C++/simple_pattern8.cpp
@@ -4,7 +4,7 @@ int main()
 {
     for(int row=1;row<=5;row++)
     {
-        for(char col='a';col<='e';col++)
+        for(char col='a';col<='e';col++) 
         {
             cout<<col<<" ";//a,b,c,d,e print hoga 5 row me 
         }
---
+++ C++/simple_pattern9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int count =1;
+     for(int i=1;i<=5;i++)
+     {
+        for(int j=1;j<=5;j++)
+        {
+            cout<<count<<" ";//1 ,2,3,4,5 aise hi 5 row print hoga number 25 tak
+            
+            count++;
+        }
+        cout<<endl;
+     }
+}
