54d2fc4128644e1515185a34065e23a8425bc3cf
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-09 21:05:30 +0530

    simple_pattern11

--- C++/simple_pattern9.cpp
+++ C++/simple_pattern9.cpp
@@ -5,12 +5,12 @@ int main()
      int count =1;
      for(int i=1;i<=5;i++)
      {
-        for(int j=1;j<=5;j++)
+        for(int j=1;j<=5;j++)   
         {
             cout<<count<<" ";//1 ,2,3,4,5 aise hi 5 row print hoga number 25 tak
             
-            count++;
+            count++;  
         }
-        cout<<endl;
+        cout<<endl;  
      }
 }
