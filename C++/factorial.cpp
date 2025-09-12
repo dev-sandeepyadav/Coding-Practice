4a65b1073cb0384d30d467348a02b1f9285f9db6
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-12 19:20:35 +0530

    hard_pattern1

--- C++/factorial.cpp
+++ C++/factorial.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-
+#include<iostream>
+using namespace std ;
 int main()
 {
-   int n,fact=1;
-   cout<<"enter your number: ";
+   int n, fact=1;
+   cout<<"enter your number :";
    cin>>n;
    for(int i=1;i<=n;i++)
    {
-    fact= fact*i;
+      fact =fact*i;
    }
-   cout<<fact;
-   
+   cout<<fact<<" ";
+
+
 }
