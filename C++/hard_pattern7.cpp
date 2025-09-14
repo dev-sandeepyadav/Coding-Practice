4b2fde7ddc9a021d6dd3bf507ddd659e143eb065
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-14 12:18:41 +0530

    hard_pattern7

--- C++/hard_pattern7.cpp
+++ C++/hard_pattern7.cpp
@@ -3,19 +3,20 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"user input number ";
+    cout<<"input number ";
     cin>>n;
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n-i;j++)
         {
             cout<<"  ";
-        }
-        for(int k=1;k<=2*i-1;k++)
-        {
-            cout<<"* ";
 
         }
+        for(int j=1;j<=i;j++)
+        cout<<j<<" ";
+        for(int j=i-1;j>=1;j--)
+        cout<<j<<" ";
         cout<<endl;
     }
+    
 }
