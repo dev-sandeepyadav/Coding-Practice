bbfb26497501c3d910fc88da7f2275c83b183569
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-18 08:03:09 +0530

    anynumber_of_table

---
+++ whileloop_c++/anynumber_of_table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number whose table you want: ";
+    cin>>n;int i=1;
+    while(i<=10)
+    {
+        cout<<n*i<<endl;
+        i++;
+    }
+}
