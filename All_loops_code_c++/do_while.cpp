07565c14712387f4ff93440760787cf44fbcf12f
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-18 08:29:03 +0530

    do_while

---
+++ All_loops_code_c++/do_while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"enter your number : ";
+    cin>>n;
+    
+   do{
+    cout<<i<<" ";
+    i++;
+   }while(i<=n);
+   
+   
+}
