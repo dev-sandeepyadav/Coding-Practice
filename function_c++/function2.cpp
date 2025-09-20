96087d26db21857c2ff099651dc220ace9e510a6
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-20 12:50:01 +0530

    function2

---
+++ function_c++/function2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void gift(){
+    cout<<"have a nice day"<<endl;;
+    cout<<"good morning "<<endl;
+}
+
+
+int main()
+    {
+       gift();//function call 
+       gift();
+    }
+//sabse pahle uper ka 2 lib e chalega our fir direct main function me jayega usame dekha gift ko print karana hai to 
+//o fir void ke ander jayega our usake ander jo hoga usko print kr dega 
