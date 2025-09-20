2bf51ce74b565fb7ea5615ff36650602646ada54
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-20 14:45:42 +0530

    function_Q1

---
+++ function_c++/function_Q1.cpp
@@ -0,0 +1,41 @@
+
+//Q1... find combination and permutation using function ? 
+
+
+#include<iostream>
+
+#include<cmath>
+using namespace std;
+   int fact(int x){
+    int f =1;
+    for(int i=2;i<=x;i++){
+        f = f*i;
+    }
+    return f;
+   }
+   int combination ( int n,int r){
+    int ncr = fact(n)/(fact(r)*fact(n-r));
+    return ncr;
+   }
+   int permutation( int n,int r){
+    int npr = fact(n)/fact(r);
+    return npr ;
+        }
+
+
+
+int main(){
+int n,r;
+cout<<"enter of your number n :";
+cin>>n;
+cout<<"enter your number r : ";
+cin>>r;
+int ncr = combination (n,r);
+int npr = permutation(n,r);
+cout<<ncr<<endl<<npr;
+
+
+
+
+
+}
