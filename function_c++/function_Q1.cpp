7995a3d40528167bae8f31e9b096ddfefc6097af
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-09-20 16:02:33 +0530

    function_Q2

--- function_c++/function_Q1.cpp
+++ function_c++/function_Q1.cpp
@@ -9,9 +9,9 @@ using namespace std;
    int fact(int x){
     int f =1;
     for(int i=2;i<=x;i++){
-        f = f*i;
-    }
-    return f;
+       f = f*i;
+  }
+   return f;
    }
    int combination ( int n,int r){
     int ncr = fact(n)/(fact(r)*fact(n-r));
