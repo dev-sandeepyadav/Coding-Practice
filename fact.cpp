82b0c2ddb04f9f46cfa4ae5492d2d9db4a91c46a
dev-sandeepyadav <sandeepk060709@gmail.com>
2025-10-02 11:00:51 +0530

    basicQ

---
+++ fact.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void fun(int x,int y){
+    cout<<"void address  "<<&x<<endl;
+    cout<<"void address  "<<&y;
+}
+
+
+
+int main(){
+    int x = 4;
+    int y=6;
+    cout<<&x<<endl;
+    cout<<&y<<endl;
+    fun(x,y);
+}
